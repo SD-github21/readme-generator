06f9da57330a6d1e43456b4fc6f830e07284ad39
SD-github21 <sapandonde.gmail.com>
2022-05-12 22:22:32 -0500

    add README.md template and code to write README file

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js")
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('./dist/'+ fileName , generateMarkdown(data), err => {
+        if (err) throw err;
+
+        console.log('README generation complete! Check out README.md to see the ouput!');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+    .then(data => {
+
+    })
+}
 
 // Function call to initialize app
 init();
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,37 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # **${data.title}**
+
+  ## **Description**
+  ${data.description}
+
+  ## **Table of Contents**
+  [Installation](https://github.com/SD-github21/readme-generator#installation)
+  [Usage](https://github.com/SD-github21/readme-generator#usage)
+  [License](https://github.com/SD-github21/readme-generator#license)
+  [Contributing](https://github.com/SD-github21/readme-generator#contributing)
+  [Tests](https://github.com/SD-github21/readme-generator#tests)
+  [Questions](https://github.com/SD-github21/readme-generator#questions)
+
+  ## **Installation**
+  ${data.installation}
+
+  ## **Usage**
+  ${data.usage}
+
+  ## **License**
+  ${data.license}
+
+  ## **Contributing**
+  ${data.contributing}
+
+  ## **Tests**
+  ${data.tests}
+
+  ## **Questions**
+  ${data.questions}
 
 `;
 }
